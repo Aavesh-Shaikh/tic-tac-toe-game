482bdb9fe4c72d02d2048b83ff1bb5ae84a8b267
Rahil Shaikh <shaikhrahil2397@gmail.com>
2023-12-30 00:14:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
+
+function App() {
+  return <TicTacToe />;
+}
+
+export default App;
---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,167 @@
+import React, { useRef, useState } from "react";
+import "./TicTacToe.css";
+import circle_icon from "../Assets/circle.png";
+import cross_icon from "../Assets/cross.png";
+import fireCrackerSound from "../../Sounds/firework-show-short-64657.mp3";
+let data = ["", "", "", "", "", "", "", "", ""];
+
+function TicTacToe() {
+  const titleRef = useRef(null);
+  let box1 = useRef(null);
+  let box2 = useRef(null);
+  let box3 = useRef(null);
+  let box4 = useRef(null);
+  let box5 = useRef(null);
+  let box6 = useRef(null);
+  let box7 = useRef(null);
+  let box8 = useRef(null);
+  let box9 = useRef(null);
+
+  let boxArray = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
+  const [count, setCount] = useState(0);
+
+  const [lock, setLock] = useState(false);
+
+  const toggle = (e, indexNum) => {
+    if (lock) {
+      return 0;
+    }
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src='${cross_icon}' alt="cross_icon" /> `;
+      data[indexNum] = "x";
+      setCount((prevCount) => prevCount + 1);
+    } else {
+      e.target.innerHTML = `<img src='${circle_icon}' alt="circle_icon" /> `;
+      data[indexNum] = "o";
+      setCount((prevCount) => prevCount + 1);
+    }
+    checkWin();
+  };
+
+  const checkWin = () => {
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[2]);
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      won(data[5]);
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      won(data[8]);
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      won(data[6]);
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      won(data[7]);
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      won(data[8]);
+    } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[8]);
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      won(data[8]);
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      won(data[6]);
+    }
+  };
+
+  let audio;
+  const won = (winner) => {
+    setLock(true);
+
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congatulations : <img src=${cross_icon} alt="cross_icon" /> wins`;
+    } else {
+      titleRef.current.innerHTML = `Congatulations : <img src=${circle_icon} alt="circle_icon" /> wins`;
+    }
+
+    audio = new Audio(fireCrackerSound).play();
+  };
+
+  const resetHandler = () => {
+    setLock(false);
+    data = ["", "", "", "", "", "", "", "", ""];
+    titleRef.current.innerHTML = `Tic Tac Toe Game In <span>React</span>`;
+    boxArray?.map((e) => {
+      e.current.innerHTML = "";
+    });
+  };
+  return (
+    <div className="tic_tac_game_main_div">
+      <h1 className="title" ref={titleRef}>
+        Tic Tac Toe Game In <span>React</span>
+      </h1>
+      <div className="board">
+        <div className="row1">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 0);
+            }}
+            ref={box1}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 1);
+            }}
+            ref={box2}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 2);
+            }}
+            ref={box3}
+          ></div>
+        </div>
+        <div className="row2">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 3);
+            }}
+            ref={box4}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 4);
+            }}
+            ref={box5}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 5);
+            }}
+            ref={box6}
+          ></div>
+        </div>
+        <div className="row3">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 6);
+            }}
+            ref={box7}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 7);
+            }}
+            ref={box8}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 8);
+            }}
+            ref={box9}
+          ></div>
+        </div>
+      </div>
+      <button className="reset_btn" onClick={resetHandler}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default TicTacToe;
